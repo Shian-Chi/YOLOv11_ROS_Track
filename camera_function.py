202f2567c803c60130e696fb4322312c96f0ed44
Shian-Chi <siancicheng@gmail.com>
2025-05-22 19:18:20 +0800

    stable

--- camera_function.py
+++ camera_function.py
@@ -6,7 +6,7 @@ from utils import check_imshow, increment_path, current_path
 def gstreamer_pipeline_2(
     sensor_id=0,
     capture_width=1920, capture_height=1080,
-    display_width=960,  display_height=540,
+    display_width=1280,  display_height=720,
     framerate=25,       # 保持 25 fps
     flip_method=0):
     return (
@@ -30,40 +30,6 @@ def gstreamer_pipeline_2(
            capture_width, capture_height, framerate,
            flip_method, display_width, display_height)
     )
-
-def gstreamer_pipeline_1(
-    sensor_id=0,
-    capture_width=1920, capture_height=1080,
-    display_width=960,  display_height=540,
-    framerate=30,       flip_method=0):
-
-    return (
-        # ★★ 相機 ★★
-        "nvarguscamerasrc sensor-id=%d "
-        "exposuretimerange=\"34000 34000\" "      # >= 34000 µs 才合法
-        "gainrange=\"1 1\" "                      # 最低類比增益抑制亮度
-        "wbmode=1 "                              # daylight
-        "saturation=1.3 "                        # nvarguscamerasrc 支援
-        "aelock=true ! "
-
-        # ★★ ISP → CPU ★★
-        "video/x-raw(memory:NVMM), width=(int)%d, height=(int)%d, "
-        "framerate=(fraction)%d/1 ! "
-        "nvvidconv flip-method=%d ! "
-
-        # ★★ 影像後製 （這裡才有 contrast）★★
-        "videobalance contrast=1.2 brightness=-0.1 ! "
-
-        # ★★ 送到 OpenCV ★★
-        "video/x-raw, width=(int)%d, height=(int)%d, format=(string)BGRx ! "
-        "videoconvert ! "
-        "video/x-raw, format=(string)BGR ! appsink"
-        % (sensor_id,
-           capture_width, capture_height, framerate,
-           flip_method,
-           display_width, display_height)
-    )
-
 
 def gstreamer_pipeline(
     sensor_id=0,
